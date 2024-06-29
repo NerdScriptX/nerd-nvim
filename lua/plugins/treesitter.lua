13eb343d1630ae93cb658b3dd8c5f43dfed51b6d
NerdScriptX <runyshy@gmail.com>
2024-06-29 12:45:33 +0500

    Fix deprecated stuff in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,12 +11,6 @@ return {
         additional_vim_regex_highlighting = false,
       },
       indent = { enable = true },
-      autopairs = {
-        enable = true
-      },
-      autotag = {
-        enable = true
-      },
     })
   end
 }
