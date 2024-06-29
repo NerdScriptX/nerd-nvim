093708c0674bd386ed8d8e1970d8ecb501a80cd7
NerdScriptX <runyshy@gmail.com>
2024-06-29 12:50:20 +0500

    add ignore mode for node_modules into telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,24 +11,30 @@ return {
 
     -- Configuration for telescipe-ui
     require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+          "yarn.lock",
+          ".git",
+          ".sl",
+          "_build",
+          ".next",
+        },
+        hidden = true,
+        path_display = {
+          "filename_first",
+        },
+      },
       extensions = {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown {}
         }
       },
-      file_ignore_patterns = {
-        "node_modules",
-        "yarn.lock",
-        ".git",
-        ".sl",
-        "_build",
-        ".next",
-      },
     })
     require("telescope").load_extension("ui-select")
 
     -- Key mappings
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>fw', builtin.live_grep, { noremap = true, silent = true})
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>fw', builtin.live_grep, { noremap = true, silent = true })
   end
 }
